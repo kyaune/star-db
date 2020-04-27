723a18366d1de5bebedf70c11163b219e026ed84
Stas Kyaune <kyaune@gmail.com>
2020-04-27 17:55:09 +0300

    gh-pages

---
+++ precache-manifest.45b55eb76269a93dcb58adb46bc93e44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1a4b12d04be28a6a4b9b4d98ab779d8",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "5c101333228cf6acea28",
+    "url": "/star-db/static/css/main.170698ff.chunk.css"
+  },
+  {
+    "revision": "00b57557fad6704c71d4",
+    "url": "/star-db/static/js/2.8016df20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-db/static/js/2.8016df20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c101333228cf6acea28",
+    "url": "/star-db/static/js/main.54df29b7.chunk.js"
+  },
+  {
+    "revision": "43fc4191a72fe93fae22",
+    "url": "/star-db/static/js/runtime-main.d820b30c.js"
+  }
+]);
